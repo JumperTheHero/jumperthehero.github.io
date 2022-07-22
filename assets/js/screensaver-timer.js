68ff7004415842af7b4d6dc44af3b3a0521be5f0
JumperTheHero <109250665+JumperTheHero@users.noreply.github.com>
2022-07-21 20:19:03 -0500

    DQ 35

--- assets/js/screensaver-timer.js
+++ assets/js/screensaver-timer.js
@@ -2,4 +2,4 @@ function redirectToScreensaver() {
     location.href = 'screensaver.html';
 }
 
-setTimeout(redirectToScreensaver, 600000) // set to 600000 for 10 minutes
+setTimeout(redirectToScreensaver, 45000) // set to 600000 for 10 minutes
